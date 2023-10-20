004889a1688caf22c184be069d1e5da53c6c1620
capmec <capmec@gmail.com>
2023-10-20 14:53:36 +0200

    first commit + navbar

---
+++ src/components/Nav.jsx
@@ -0,0 +1,44 @@
+import { hamburger } from '../assets/icons'
+import { headerLogo } from '../assets/images'
+import { navLinks } from '../constants'
+
+const Nav = () => {
+	return (
+		<header className='padding-x py-8 absolute z-10 w-full'>
+			<nav className='flex justify-between items-center max-container'>
+				<a
+					className='flex flex-row items-center'
+					href='/'>
+					<img
+						src={headerLogo}
+						alt='logo'
+						width={130}
+						height={29}
+					/>
+					{/* <p className='-m-12 text-md font-bold'>ShooShoes</p> */}
+				</a>
+				<ul className='flex-1 flex justify-center items-center gap-16 max-lg:hidden'>
+					{navLinks.map((item) => (
+						<li key={item.label}>
+							<a
+								href={item.href}
+								className='font-montserrat leading-normal text-lg text-slate-gray'>
+								{item.label}
+							</a>
+						</li>
+					))}
+				</ul>
+				<div className='hidden max-lg:block'>
+					<img
+						src={hamburger}
+						alt='Hamburger'
+						width={25}
+						height={25}
+					/>
+				</div>
+			</nav>
+		</header>
+	)
+}
+
+export default Nav
