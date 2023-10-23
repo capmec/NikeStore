9f97e92576cf6340b14471eeec493455972708f7
capmec <capmec@gmail.com>
2023-10-23 12:38:25 +0200

    shoe card done

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -1,3 +1,4 @@
+import { useState } from 'react'
 import Button from '../components/Button'
 import { arrowRight } from '../assets/icons'
 import { shoes, statistics } from './../constants/index'
@@ -5,14 +6,17 @@ import { bigShoe1 } from '../assets/images'
 import ShoeCard from '../components/ShoeCard'
 
 const Hero = () => {
+	const [bigShoeImg, setBigShoeImg] = useState(bigShoe1)
+
 	return (
 		<section
 			id='home'
-			className='w-full flex xl:flex-row flex-col justify-center min-h-screen gap-10 max-container border-2 border-red-500 p-2'>
+			className='w-full flex xl:flex-row flex-col justify-center min-h-screen gap-10 max-container'>
 			<div className='relative xl:w-2/5 flex flex-col justify-center items-start w-full max-xl:padding-x pt-28'>
 				<p className='text-xl font-montserrat text-coral-red'>
 					Our Sommer Collection
 				</p>
+
 				<h1 className='mt-10 font-palanquin text-8xl max-sm:text-[72px] max-sm:leading-[82px] font-bold'>
 					<span className='xl:bg-white xl:whitespace-nowrap relative z-10 pr-10'>
 						The new Arrival
@@ -24,14 +28,15 @@ const Hero = () => {
 					Discover stylish Nike arrivals, quality comfort and innovation for
 					your active life.
 				</p>
+
 				<Button
 					label='Shop now'
 					iconURL={arrowRight}
 				/>
 
 				<div className='flex justify-start items-start flex-wrap w-full mt-20 gap-16'>
-					{statistics.map((stat) => (
-						<div>
+					{statistics.map((stat, index) => (
+						<div key={index}>
 							<p className='text-4xl font-palanquin font-bold'>{stat.value}</p>
 							<p className='leading-7 font-montserrat text-slate-gray'>
 								{stat.label}
@@ -40,21 +45,23 @@ const Hero = () => {
 					))}
 				</div>
 			</div>
+
 			<div className='relative flex-1 flex justify-center items-center xl:min-h-screen max-xl:py-40 bg-primary bg-hero bg-cover bg-center'>
 				<img
-					src={bigShoe1}
+					src={bigShoeImg}
 					alt='shoe collection'
 					width={610}
 					height={500}
 					className='object-contain relative z-10'
 				/>
-				<div>
+
+				<div className='flex sm:gap-6 gap-4 absolute -bottom-[5%] sm:left-[10%] max-sm:px-6'>
 					{shoes.map((shoe) => (
-						<div>
+						<div key={shoe}>
 							<ShoeCard
 								imgURL={shoe}
-								changeBigShoeImage={() => {}}
-								bigShoeImg=''
+								changeBigShoeImage={(shoe) => setBigShoeImg(shoe)}
+								bigShoeImg={bigShoeImg}
 							/>
 						</div>
 					))}
